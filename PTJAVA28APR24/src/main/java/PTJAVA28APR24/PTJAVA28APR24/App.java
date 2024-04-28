e65893b8a2821b1fa8d7b025d507f62998d070d3
bhavins <getbhavin84@gmail.com>
2024-04-28 18:19:20 +0530

    oktest

---
+++ PTJAVA28APR24/src/main/java/PTJAVA28APR24/PTJAVA28APR24/App.java
@@ -0,0 +1,30 @@
+package PTJAVA28APR24.PTJAVA28APR24;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ){
+    
+    	
+   	byte age;
+	age = 24;
+	
+	short marks = 500;
+	
+	int salary = 40000;
+	
+	long turnover = 100000000;
+	
+	System.out.println("Hello Selenium");
+	
+	System.out.println(age);
+	System.out.println(marks);
+	System.out.println("Salary: "+salary);
+	System.out.println(age+marks);
+	
+	System.out.println(turnover);
+}
+}
